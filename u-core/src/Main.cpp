856f5b92e4ee0c11aa401a1dcceb9d4ed334539e
Tavo <hi.gustavo.oliveira@gmail.com>
2025-12-24 13:20:42 -0300

    Initial setup

---
+++ u-core/src/Main.cpp
@@ -0,0 +1,8 @@
+#include "UEngine.h"
+
+int main()
+{
+	uei::UEngine engine;
+	engine.Update("U-engine-ish <3");
+	return 0;
+}
---
+++ u-metal-gear-solid-ish/src/Main.cpp
@@ -0,0 +1,8 @@
+#include "UEngine.h"
+
+int main()
+{
+	uei::UEngine engine;
+	engine.Update("U-metal-gear-solid-ish <3");
+	return 0;
+}
